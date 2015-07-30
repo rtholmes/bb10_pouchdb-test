a0d0d327444939ef88d9efd1d16deb629d469087
Reid Holmes (MBA) <rtholmes@gmail.com>
2015-07-30 14:12:05 -0700

    initial commit

---
+++ www/js/test.js
@@ -0,0 +1,56 @@
+function getDb() {
+	// print("getDb() - start");
+    return new PouchDB('tileDB', {adapter: 'idb'});
+    // return new PouchDB('tileDB', {adapter: 'websql', size: 100});
+}
+
+function destroyDb() {
+	print("destroyDb() - starting...");
+	
+    var db = getDb();
+    db.destroy().then(function () {
+        print("destroyDb() - success");
+    }).catch(function (err) {
+        print("destroyDb() - ERROR: " + err);
+    });
+}
+
+function print(msg) {
+	console.log(msg);
+	var ele = document.getElementById('output');
+	ele.innerHTML = msg+"<br/>"+ele.innerHTML;
+}
+
+function testDb() {
+	// print("testDb() - start")
+    var key = "KEY_" + performance.now();
+
+    if (typeof window.testDbValue === 'undefined') {
+		print("testDb() - start");
+        window.testDbValue = "";
+        window.testDbIteration = 0;
+        for (var i = 0; i < 262144; i++) { // 256 kb
+            window.testDbValue = window.testDbValue + "A";
+        }
+    }
+    var value = window.testDbValue;
+
+    var db = getDb();
+    try {
+        var tile = {_id: key, data: value};
+        db.put(tile, function callback(err, result) {
+            if (!err) {
+                window.testDbIteration++;
+                print('testDb(..) - Success; stored: ' + (window.testDbIteration / 4).toFixed(1) + " MB");
+                testDb();
+            } else {
+                var reason = err.name + "_" + err.message + "_" + err.reason;
+                print('testDb(..) - ERROR ( ' + err.status + " ) : " + JSON.stringify(err));
+                delete window.testDbValue;
+                delete window.testDbIteration;
+            }
+        });
+    } catch (err) {
+        print("testDb(..) - ERROR: " + err.message);
+    }
+}
